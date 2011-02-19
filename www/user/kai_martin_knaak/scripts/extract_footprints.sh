d21b1431eb6c5a455cd9e6177892740d76913996
kai_martin_knaak <>
2011-02-19 07:20:23 +0000

    Modified Files: 	changelog.html extract_footprints.sh index.html 	footprints/generic/DPAK.fp footprints/generic/PENTAWATTV.fp 	footprints/generic/PENTAWATTV.pcb 	footprints/generic/PENTAWATTV_LAY_2.fp 	footprints/generic/SO14.fp footprints/generic/SO16.fp 	footprints/generic/SO28W.fp footprints/generic/SO8.fp 	footprints/generic/SO8_FET.fp footprints/generic/SOT223.fp 	footprints/generic/SOT23.fp footprints/generic/SOT23_BEC.fp 	footprints/generic/SOT23_GSD.fp 	footprints/generic/SOT23_SDG.fp footprints/generic/SOT343.fp 	footprints/generic/SOT363.fp footprints/generic/SOT89.fp 	footprints/generic/SOxx.pcb footprints/generic/TFQP.pcb 	footprints/generic/TO126_ECB.fp footprints/generic/TO18.fp 	footprints/generic/TO220.fp footprints/generic/TO220_ECB.fp 	footprints/generic/TO220_ECB_LAY.fp 	footprints/generic/TO220_GDS.fp 	footprints/generic/TO220_GDS_LAY.fp 	footprints/generic/TO220_LAY.fp 	footprints/generic/TO220_bighole.fp 	footprints/generic/TO220_block.fp footprints/generic/TO247.fp 	footprints/generic/TO247_2.fp footprints/generic/TO247_2PIN.fp 	footprints/generic/TO247_2PIN_2.fp 	footprints/generic/TO247_2PIN_LAY.fp 	footprints/generic/TO247_LAY.fp footprints/generic/TO252.fp 	footprints/generic/TO92_123.fp footprints/generic/TO92_DGS.fp 	footprints/generic/TO92_ECB.fp footprints/generic/TO92_GDS.fp 	footprints/generic/TOXXX.pcb footprints/generic/TSOP6.fp 	footprints/generic/TSOPxx.pcb footprints/specific/BPW34.pcb Added Files: 	set_name_value_description.sh

--- www/user/kai_martin_knaak/scripts/extract_footprints.sh
+++ www/user/kai_martin_knaak/scripts/extract_footprints.sh
@@ -6,7 +6,14 @@
 awk '
    BEGIN { FS = "\"" }        # use " as field separator
    $1 ~ /Element\[/ {         # if the line is an Element statement
-      fp_name = $8".fp"       # extract footprint name from Element statement
+      fp_name = ""
+      if ($8 != "" ) fp_name = $8".fp"       # extract footprint value or 
+      if ($6 != "" ) fp_name = $6".fp"       # name or
+      if ($4 != "" ) fp_name = $4".fp"       # description from Element statement
+      if ( fp_name == "" ) {
+                 print "Footprint without a name: "$0
+                 next
+                 }
       print $0 > fp_name      # print line with Element statement to file
       getline                 # get the next line 
       while ( $1 != "\t)" ) { # while line does not start with tab followed by )
@@ -14,5 +21,6 @@ awk '
         getline               # get the next line
       }                       # end of while loop
       print $0 > fp_name      # append the last line
+      printf ("extracted %s\n", fp_name) 
    }
 ' $1
---
+++ www/user/kai_martin_knaak/scripts/set_name_value_description.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# This little script uses awk to set name, value and description of footprints 
+# to the same string. This is useful for footprints in the library.
+################################################# -<)kmk(>---<(2011)
+
+awk -v filename="$1" '
+   BEGIN { FS = " " }        # use space as field separator
+{   
+   if ( $1 ~ /Element\[/ ) {         # if the line is an Element statement
+      fp_name = ""
+      if ($4 != "\"\"" ) fp_name = $4    # extract footprint value or 
+      if ($3 != "\"\"" ) fp_name = $3    # name or
+      if ($2 != "\"\"" ) fp_name = $2    # description from Element statement
+      if ( fp_name == "" ) {
+                 print "Footprint without a name: "$0
+                 next                     # immediately look for next Element
+                 }
+      $4 = fp_name                        # set description,
+      $3 = fp_name                        # set name
+      $2 = fp_name                        # set value
+      printf ("normalized %s\n", fp_name) 
+   }
+   print $0 > "/tmp/"filename      # print to tmp file
+}
+' $1
+
+mv $1 /tmp/$1.backup
+mv /tmp/$1 $1
