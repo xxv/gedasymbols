91e5d77c766c14dd1cc70da621bae5de9b30fc77
kai_martin_knaak <>
2011-07-06 03:15:14 +0000

    *** empty log message ***

--- www/user/kai_martin_knaak/scripts/copy-to-essential.sh
+++ www/user/kai_martin_knaak/scripts/copy-to-essential.sh
@@ -2,6 +2,9 @@
 # A very simple script to copy symbols and components to the essential lib
 
 PRE=essential/symbols
+cp symbols/titleblock/title-block.sym $PRE/titleblock/titleblock.sym
+cp symbols/titleblock/in.sym $PRE/titleblock
+cp symbols/titleblock/out.sym $PRE/titleblock
 
 PRE=essential/footprints
 cp footprints/discrete/res_RM10.fp $PRE/discrete
