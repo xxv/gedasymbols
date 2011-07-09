3ff8df025edae70e5e2a6f1e6eece4baa993ffbc
kai_martin_knaak <>
2011-07-09 15:08:58 +0000

    ----------------------------------------------------------------------  Enter Log.  Lines beginning with `' are removed automatically
     Committing in .
    
     Modified Files:
     	changelog.html copy-to-essential.sh essential/changelog.html
     	essential/footprints/discrete/res_RM10.fp
     	essential/footprints/discrete/res_RM15.fp
     	essential/footprints/discrete/res_RM20.fp
     	essential/footprints/discrete/res_vert.fp
     	essential/footprints/generic/0603.fp
     	essential/footprints/generic/0805.fp
     	essential/symbols/titleblock/in.sym
     	essential/symbols/titleblock/out.sym
     	essential/symbols/titleblock/titleblock.sym
     	symbols/connector/con_2x10.sym symbols/power/12V+.sym
     	symbols/power/12V-.sym symbols/power/15V+.sym
     	symbols/power/15V-.sym symbols/power/5V+.sym
     	symbols/power/5V-.sym symbols/power/Ub+.sym
     	symbols/power/Ub-.sym symbols/power/gnd.sym
     	symbols/power/nc.sym symbols/power/rGND.sym
     	symbols/power/vcc.sym symbols/power/vref.sym
     	symbols/power/vref_minus.sym
     Added Files:
     	essential/symbols/power/5V+.sym
     	essential/symbols/power/Ub+.sym
     	essential/symbols/power/Ub-.sym
     	essential/symbols/power/gnd.sym essential/symbols/power/nc.sym
     	essential/symbols/power/vcc.sym
     	essential/symbols/power/vee.sym
     	symbols/connector/con_2x10a.sym symbols/power/vee.sym
     Removed Files:
     	symbols/power/vcc-minus.sym
     ----------------------------------------------------------------------

--- www/user/kai_martin_knaak/scripts/copy-to-essential.sh
+++ www/user/kai_martin_knaak/scripts/copy-to-essential.sh
@@ -6,6 +6,15 @@ cp symbols/titleblock/title-block.sym $PRE/titleblock/titleblock.sym
 cp symbols/titleblock/in.sym $PRE/titleblock
 cp symbols/titleblock/out.sym $PRE/titleblock
 
+cp symbols/power/gnd.sym $PRE/power
+cp symbols/power/vcc.sym $PRE/power
+cp symbols/power/vee.sym $PRE/power
+cp symbols/power/Ub+.sym $PRE/power
+cp symbols/power/Ub-.sym $PRE/power
+cp symbols/power/5V+.sym $PRE/power
+cp symbols/power/nc.sym $PRE/power
+
+
 PRE=essential/footprints
 cp footprints/discrete/res_RM10.fp $PRE/discrete
 cp footprints/discrete/res_RM15.fp $PRE/discrete
