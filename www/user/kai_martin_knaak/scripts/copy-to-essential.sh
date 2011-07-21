1422701b83b3322e7d53b92daae00964d0514cfa
kai_martin_knaak <>
2011-07-21 00:31:48 +0000

    Modified Files: 	copy-to-essential.sh essential/changelog.html 	essential/symbols.html essential/symbols/power/vcc.sym 	essential/symbols/power/vee.sym footprints/discrete/cap.pcb 	footprints/discrete/cap_RM275.fp 	footprints/discrete/cap_RM5.fp 	footprints/discrete/cap_RM5_b.fp 	footprints/discrete/cap_RM5_c.fp 	symbols/analog/cap_SMD_vert.sym Added Files: 	essential/symbols/discrete/cap_vert.sym 	footprints/discrete/cap_100mil.fp 	footprints/discrete/cap_1100mil.fp 	footprints/discrete/cap_200mil.fp 	footprints/discrete/cap_200mil_a.fp 	footprints/discrete/cap_200mil_b.fp 	footprints/discrete/cap_RM5_a.fp symbols/analog/cap_vert.sym ----------------------------------------------------------------------

--- www/user/kai_martin_knaak/scripts/copy-to-essential.sh
+++ www/user/kai_martin_knaak/scripts/copy-to-essential.sh
@@ -8,9 +8,11 @@ cp symbols/analog/opamp_pwr.sym $PRE/analog
 cp symbols/analog/opamp_dual.sym $PRE/analog
 cp symbols/analog/opamp_dual_pwr.sym $PRE/analog
 cp symbols/analog/opamp_offset.sym $PRE/analog
-
 cp symbols/analog/igbt.sym $PRE/discrete
 
+cp symbols/analog/cap_vert.sym $PRE/discrete
+# cp symbols/analog/res.sym $PRE/discrete
+# cp symbols/analog/res2.sym $PRE/discrete
 
 cp symbols/titleblock/title-block.sym $PRE/titleblock/titleblock.sym
 cp symbols/titleblock/in.sym $PRE/titleblock
@@ -24,12 +26,19 @@ cp symbols/power/Ub-.sym $PRE/power
 cp symbols/power/5V+.sym $PRE/power
 cp symbols/power/nc.sym $PRE/power
 
+cp symbols/titleblock/title-block.sym $PRE/titleblock/titleblock.sym
+cp symbols/titleblock/in.sym $PRE/titleblock
+cp symbols/titleblock/out.sym $PRE/titleblock
 
 PRE=essential/footprints
 cp footprints/discrete/res_RM10.fp $PRE/discrete
 cp footprints/discrete/res_RM15.fp $PRE/discrete
 cp footprints/discrete/res_RM20.fp $PRE/discrete
 cp footprints/discrete/res_vert.fp $PRE/discrete
+
+cp footprints/discrete/cap_200mil.fp $PRE/discrete
+cp footprints/discrete/cap_200mil_a.fp $PRE/discrete
+cp footprints/discrete/cap_100mil.fp $PRE/discrete
 
 cp footprints/generic/0805.fp $PRE/generic
 cp footprints/generic/0603.fp $PRE/generic
@@ -38,3 +47,4 @@ cp footprints/generic/TO220.fp $PRE/generic
 cp footprints/generic/TO220_GDS.fp $PRE/generic
 cp footprints/generic/TO220_GCE.fp $PRE/generic
 cp footprints/generic/TO220_ECB.fp $PRE/generic
+
