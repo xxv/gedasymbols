d9d8ae6fa359520c86288090c12e6a56d769d9ee
kai_martin_knaak <>
2012-07-25 18:27:28 +0000

    Modified Files: 	changelog.html copy-to-essential.sh edit_library_metafiles.sh 	symbols/templates/subsheet-2.sym 	symbols/templates/subsheet.sym ----------------------------------------------------------------------

--- www/user/kai_martin_knaak/scripts/edit_library_metafiles.sh
+++ www/user/kai_martin_knaak/scripts/edit_library_metafiles.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # a mini convenience script to 
+cd $HOME/geda/gedasymbols/kai_martin_knaak
 gedit changelog.html \
       symbols.html \
       footprints.html \
       essential/changelog.html \
       essential/footprints.html \
       essential/symbols.html \
-      copy-to-essential.sh
+      copy-to-essential.sh &
