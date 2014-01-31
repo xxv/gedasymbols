1c138c6faeb73d214fa42f8ab8882bb61ef76c98
alexey_kharikovskiy <>
2014-01-31 14:23:51 +0000

    Add suport of slotted and splitted components for refdes_renum.py

--- www/user/alexey_kharikovskiy/tools/refdes_renum.py
+++ www/user/alexey_kharikovskiy/tools/refdes_renum.py
@@ -12,6 +12,9 @@ designators.  The reference designators are numbered starting with 1 and the
 old schematic file is replaced by the modified schematic file. Also replaced REFDESs in pcb file(s) accordingly to schematic file(s).
 refdes_renum.py can read the project file(files) and renumber all REFDESs in project.
 
+Warning: For properly renumber of slotted and complex(many symbols) components You must place identical REFDES to him. 
+Example: U1, U1 or U101, U101, U101
+
 refdes_renum.py accepts the following options:
 
     --help      Displays this help message.
@@ -211,7 +214,7 @@ for i in sys.argv[1:]:
 #When in project file "output-name foo.pcb", gsch2pcb create  foo.pcb.pcb, therefore this file IS main PCB.
 				if temp not in pcbnames:
 					pcbnames.append(temp)
-# Read content of PCB files 
+# Read content of PCB file
 for i in pcbnames:
 	file=open(i,'rt')
 	pcbcontent.append(file.read())
@@ -219,7 +222,7 @@ for i in pcbnames:
 # Read content of PCB files 
 
 page_num = 0
-for filename in schem_names: 
+for filename in schem_names: # This 
 	#Read content SCH files one at a time!!! In future may be replace for read content all SCH files
 	file=open(filename,'rt')
 	content = file.read()
@@ -274,7 +277,7 @@ for filename in schem_names:
 		#Example:			[-50800 , 65100 , 'C 65100 50800 ... resistor-1.sym {....refdes=R12...}']
 		
 		#Find the replaced REFDES letter prefix and number suffix and ADD to component list 
-		refdes=re.search('(.*)%s=([A-Z]*)(\d+|\?*)' %(attr) ,comp_list[i][-1],flags=re.DOTALL)
+		refdes=re.search('(.*)%s=([a-zA-Z]*)(\d+|\?*)' %(attr) ,comp_list[i][-1],flags=re.DOTALL)
 		comp_list[i].insert(0,refdes.group(2))
 		comp_list[i].append(refdes.group(3) if refdes.group(3) else '') #For letters only REFDESs (in future We can remove they from recplacing list)
 
@@ -285,43 +288,48 @@ for filename in schem_names:
 	# Sorting the replaced component/refdes list with REFDESs prefix and after with selected sort algorithm
 	comp_list = sorted(comp_list)
 	
-	ref_curr_pref=''
+	ref_curr_pref='0' #This is NON alfabetic for available renumber of empty refdess (e.g. "1", "2", "234")
 	if 'ref_last_nums' not in locals():
 		ref_last_nums={} #Lasts numbers for REFDESs previous schematic (For discontinuous renumber (without --pgskip)) 
 		#Example of this dict content: {'DD':5, 'R':14, 'C':45}
 
-	for i in comp_list: #Replacing sorted attributes
-		#Complex calulate of current REFDES 
-		if ref_curr_pref == i[0]:
-			ref_curr_num += 1
+	for i in range(len(comp_list)): #Replacing sorted attributes
+		#Complex calculate of current number of attribute
+		if ref_curr_pref == comp_list[i][0]: # This Attribure (with current prefix) is not first in this Schematic file
+			if comp_list[i][~0]!='?' and comp_list[i][~0] in map(lambda x:x[~1],comp_list[i_first_prefix:i]): #Repeating attribute, e.g New SLOT of existing REFDES)
+				comp_list[i].append(str(comp_list[i_first_prefix+map(lambda x:x[~1],comp_list[i_first_prefix:i]).index(comp_list[i][~0])][~0])) #Add previous number of this symbol for this SLOT or PART(for many symbols component)
+			else: #Next number attribute
+				ref_curr_num += 1
+				comp_list[i].append(str(ref_curr_num)) #Add current number to component list
 		else:
-			try:ref_curr_num = page_num + 1 if page_num != 0 else ref_last_nums[i[0]] + 1 
+			try:ref_curr_num = page_num + 1 if page_num != 0 else ref_last_nums[comp_list[i][0]] + 1 
 			except:ref_curr_num=1
-			ref_curr_pref = i[0]
+			comp_list[i].append(str(ref_curr_num)) #Add current number to component list
+			ref_curr_pref = comp_list[i][0]
+                        i_first_prefix = i # Begin of current prefix
 		ref_last_nums[ref_curr_pref]=ref_curr_num # Add current REFDES in last numbers
-		i.append(str(ref_curr_num)) #Add current number to component list
 
 		#comp_list content: ['REFDES prefix', coord1, coord2, 'replaced string of file'				,'Old NUMBER','New NUMBER']
 		#Example:			[  'R'			, -50800 , 65100 , 'C 65100 50800 ...{....refdes=R12...},	'12'	 ,	'4']
 		
-		content = content.replace(i[~2],i[~2].replace('%s=' %(attr) +i[0]+i[~1],'%s=' %(attr) +i[0]+i[~0])) #Replacing current string with New REFDES
+		content = content.replace(comp_list[i][~2],comp_list[i][~2].replace('%s=' %(attr) +comp_list[i][0]+comp_list[i][~1],'%s=' %(attr) +comp_list[i][0]+comp_list[i][~0])) #Replacing current string with New REFDES
 		if log and verbose:
-			print 'Replacing in sch : ', i[0]+i[~1], ' => ', i[0]+i[~0]
-			print '<<< ', i[~2]
-			print '>>> ', i[~2].replace('%s=' %(attr) +i[0]+i[~1],'%s=' %(attr) +i[0]+i[~0])
+			print 'Replacing in sch : ', comp_list[i][0]+comp_list[i][~1], ' => ', comp_list[i][0]+comp_list[i][~0]
+			print '<<< ', comp_list[i][~2]
+			print '>>> ', comp_list[i][~2].replace('%s=' %(attr) +comp_list[i][0]+comp_list[i][~1],'%s=' %(attr) +comp_list[i][0]+comp_list[i][~0])
 			print '------------------------------------'
 
 		if attr=='refdes':
 			for pcb_index in range(0,len(pcbcontent)): #Search current REFDES in pcb files and ADD context of REFDES (appropriate element ontex completely) to list of replaces
-				elem = re.search('Element\[[^[\]]*"%s"[^[\]]*\]' %(i[0]+i[~1]),pcbcontent[pcb_index] ,flags=re.DOTALL)
+				elem = re.search('Element\[[^[\]]*"%s"[^[\]]*\]' %(comp_list[i][0]+comp_list[i][~1]),pcbcontent[pcb_index] ,flags=re.DOTALL)
 				if elem:
 					try:
-						pcb_replaces.append([pcb_index,elem.group(),elem.group().replace(i[0]+i[~1],i[0]+i[~0])]) 
-					except:pcb_replaces =  [[pcb_index,elem.group(),elem.group().replace(i[0]+i[~1],i[0]+i[~0])]] #Create new replace list
+						pcb_replaces.append([pcb_index,elem.group(),elem.group().replace(comp_list[i][0]+comp_list[i][~1],comp_list[i][0]+comp_list[i][~0])]) 
+					except:pcb_replaces =  [[pcb_index,elem.group(),elem.group().replace(comp_list[i][0]+comp_list[i][~1],comp_list[i][0]+comp_list[i][~0])]] #Create new replace list
 					if log and verbose:
-						print 'Replacing in pcb: ', i[0]+i[~1], ' => ', i[0]+i[~0]
+						print 'Replacing in pcb: ', comp_list[i][0]+comp_list[i][~1], ' => ', comp_list[i][0]+comp_list[i][~0]
 						print '<<< ', elem.group()
-						print '>>> ', elem.group().replace(i[0]+i[~1],i[0]+i[~0])
+						print '>>> ', elem.group().replace(comp_list[i][0]+comp_list[i][~1],comp_list[i][0]+comp_list[i][~0])
 						print '------------------------------------'
 
 	if 'pcb_replaces' in vars(): #If PCB replace list are exists
